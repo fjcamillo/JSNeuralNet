34d7404d4c6d16387a17dac11d59384733c2362f
Jerhone <fjcamillo.dev@gmail.com>
2017-06-27 19:58:42 +0800

    Create perceptron

---
+++ perceptron.js
@@ -0,0 +1,13 @@
+const weights = {
+    w1: ,
+    w2: ,
+}
+const bias = {
+    b1: 
+}
+
+export const perceptron(weights, bias, x) => {
+    logit = weight*x + bias 
+    softmax = 1 / 1 + math.e**-logit
+    return softmax
+}
